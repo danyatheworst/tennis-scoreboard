a9ebcebb059205c8291d93236041526f4d73da06
danyatheworst <danyatheworst@gmail.com>
2024-06-28 15:16:08 +0300

    global and reset styles added

--- src/main/java/com/danyatheworst/home/HomeServlet.java
+++ src/main/java/com/danyatheworst/home/HomeServlet.java
@@ -9,10 +9,13 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/")
+@WebServlet("")
 public class HomeServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ThymeleafRenderer.renderFromRequest("index", req, resp);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String path = req.getPathInfo();
+        if (path == null || path.equals("/")) {
+            ThymeleafRenderer.renderFromRequest("index", req, resp);
+        }
     }
 }
